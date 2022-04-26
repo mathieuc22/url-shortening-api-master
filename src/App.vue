61eb94131a1af6cf9835d8d528ee75816ee06dc8
Mathieu <mathieu.collet@gmail.com>
2022-04-26 13:41:12 +0200

    Shorten style implementation wip

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,17 @@
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import AppHeader from './components/AppHeader.vue';
 import Hero from './components/Hero.vue';
+import Shorten from './components/Shorten.vue';
 
 </script>
 
 <template>
   <AppHeader/>
   <Hero title="More than just shorter links" />
+  <Shorten/>
 </template>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@500;700&display=swap');
-
-html {
-  font-size: 18px;
-}
-
-#app {
-  font-family: 'Poppins', sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: $color-neutral-grayishviolet;
-}
 
 .btn {
   color: #ffffff;
@@ -38,7 +27,7 @@ html {
   text-decoration: none;
   &:hover {
     color: #ffffff;
-	  background-color:#9be3e2;
+	  background-color: lighten($color: $color-primary-cyan, $amount: 20%);
 }
 }
 </style>
---
+++ src/components/Shorten.vue
@@ -0,0 +1,62 @@
+<template>
+    <section class="shorten">
+        <form action="" class="shorten__form">
+            <input type="url" class="shorten__input" name="url" id="url" placeholder="Shorten a link here..." />
+            <input type="submit" class="btn shorten__input shorten__input--submit" value="Shorten It!" />
+        </form>
+        <div class="shorten__links"></div>
+    </section>
+</template>
+
+<style lang="scss">
+
+.shorten {
+    margin-top: 100px;
+    display: flex;
+    flex-direction: column;
+    background: $color-neutral-bg;
+
+    &__form {
+        transform: translateY(-50%);
+        background-color: $color-primary-darkviolet;
+        padding: 2rem;
+        border-radius: 10px;
+        background-image: url("../assets/bg-shorten-desktop.svg");
+        display: flex;
+        justify-content: space-between;
+        gap: 1rem;
+        width: 100%;
+        font-size: 1.2rem;
+    }
+
+    &__input {
+        width: 100%;
+        padding: 0.6rem 1.5rem;
+        border-radius: 0.5rem !important;
+        border: 1px solid $color-primary-bg;
+        font-size: 1em;
+
+        &::placeholder {
+            color: $color-neutral-grayishviolet;
+        }
+
+        &:focus {
+            outline: none;
+        }
+
+        &--submit {
+            cursor: pointer;
+            border: 1px solid $color-primary-cyan;
+            flex: 1;
+            font-weight: 700;
+            transition: all 0.2s ease-in-out;
+            &:hover {
+                border: 1px solid lighten($color: $color-primary-cyan, $amount: 20%);
+            }
+            &:active {
+                transform: translateY(5px);
+            }
+        }
+    }
+}
+</style>
